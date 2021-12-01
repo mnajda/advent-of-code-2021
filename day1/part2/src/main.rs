e08a34d83499c85f49ff49219725be11bc41c85f
Mateusz Najda <mateusznajda@gmail.com>
2021-12-01 08:15:14 +0100

    added day1 part2

---
+++ day1/part2/src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs;
+
+fn load_file(path: &String) -> Vec<u64> {
+    let contents = fs::read_to_string(path).expect("Error reading file");
+    contents
+        .split_whitespace()
+        .map(|row| row.parse().unwrap())
+        .collect()
+}
+
+fn solve(input: Vec<u64>) -> usize {
+    let sums: Vec<u64> = input.windows(3).map(|pair| pair.iter().sum()).collect();
+    sums.windows(2).filter(|pair| pair[0] < pair[1]).count()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("No file provided");
+        return;
+    }
+
+    let input = load_file(&args[1]);
+    let result = solve(input);
+    println!("{}", result);
+}
