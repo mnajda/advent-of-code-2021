8db21453e015c5d0a9a9000e561f771e98e7d998
Mateusz Najda <mateusznajda@gmail.com>
2021-12-15 13:23:34 +0100

    optimized part1

--- day15/part1/main.cpp
+++ day15/part1/main.cpp
@@ -8,6 +8,8 @@
 #include <unordered_map>
 #include <vector>
 
+using PositionWithRisk = std::pair<std::int64_t, std::pair<std::int64_t, std::int64_t>>;
+
 constexpr auto steps = std::array<std::pair<std::int64_t, std::int64_t>, 4>{
     std::pair{0, 1},
     std::pair{1, 0},
@@ -42,6 +44,7 @@ std::vector<std::vector<std::int64_t>> load_file(const char* filepath)
 
 std::vector<std::pair<std::pair<std::int64_t, std::int64_t>, std::int64_t>> get_adjacent_costs(
     const std::vector<std::vector<std::int64_t>>& input,
+    const std::vector<std::vector<bool>>& visited,
     const std::size_t column,
     const std::size_t row)
 {
@@ -52,7 +55,10 @@ std::vector<std::pair<std::pair<std::int64_t, std::int64_t>, std::int64_t>> get_
         const auto x = row + step.second;
         if ((x >= 0 && x < input[column].size()) && (y >= 0 && y < input.size()))
         {
-            adjacent_points.push_back({{y, x}, input[y][x]});
+            if (!visited[y][x])
+            {
+                adjacent_points.push_back({{y, x}, input[y][x]});
+            }
         }
     }
 
@@ -61,38 +67,38 @@ std::vector<std::pair<std::pair<std::int64_t, std::int64_t>, std::int64_t>> get_
 
 std::int64_t find_shortest_path(std::vector<std::vector<std::int64_t>> input)
 {
-    auto distances = std::unordered_map<std::pair<std::int64_t, std::int64_t>, std::int64_t, pair_hash>{};
-    auto to_visit = std::queue<std::pair<std::int64_t, std::int64_t>>{};
+    auto to_visit =
+        std::priority_queue<PositionWithRisk, std::vector<PositionWithRisk>, std::greater<PositionWithRisk>>{};
+    auto visited = std::vector<std::vector<bool>>(input.size(), std::vector<bool>(input.front().size(), false));
 
-    const auto start = std::pair<std::int64_t, std::int64_t>{0, 0};
+    const auto start = PositionWithRisk{0, {0, 0}};
     const auto target = std::pair<std::int64_t, std::int64_t>{input.size() - 1, input.front().size() - 1};
 
-    distances[start] = 0;
     to_visit.push(start);
 
     while (!to_visit.empty())
     {
-        const auto point = to_visit.front();
+        const auto [current_cost, point] = to_visit.top();
         to_visit.pop();
 
         if (point == target)
         {
-            return distances[target];
+            return current_cost;
         }
 
-        const auto adjacent_points = get_adjacent_costs(input, point.first, point.second);
+        if (visited[point.first][point.second])
+        {
+            continue;
+        }
+        visited[point.first][point.second] = true;
+
+        const auto adjacent_points = get_adjacent_costs(input, visited, point.first, point.second);
         for (const auto& adjacent_point : adjacent_points)
         {
             const auto& [neighbour, cost] = adjacent_point;
 
-            const auto new_cost = distances.at(point) + cost;
-            const auto neighbour_cost = distances.find(neighbour);
-
-            if ((neighbour_cost == distances.end()) || (new_cost < neighbour_cost->second))
-            {
-                distances.insert_or_assign(neighbour, new_cost);
-                to_visit.push(neighbour);
-            }
+            const auto new_cost = current_cost + cost;
+            to_visit.push({new_cost, neighbour});
         }
     }
 
